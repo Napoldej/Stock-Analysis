4092595400785c2e50d1b824f1192ef108f7d53a
Napoldej Passornratchakul <napoldej.p@ku.th>
2024-05-02 19:26:28 +0700

    add requirements

---
+++ main.py
@@ -0,0 +1,7 @@
+from StockUi import StockUI
+from Stockcontroller import StockController
+
+if __name__ == "__main__":
+    controller = StockController()
+    main = StockUI(controller)
+    main.run()
